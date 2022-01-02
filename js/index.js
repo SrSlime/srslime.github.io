f6e72801adeaba46221f91b4997d9f04d2d5c8b8
SrSlime <68140292+SrSlime@users.noreply.github.com>
2022-01-02 02:11:39 -0300

    Updated the version of the website.
    Change a lot of things.
    V: 1.1.0
    
    - Changed the dropdown menu
    - Fixed some bugs
    - Change a little the style of the website
    - Updated the pixel dimension project website. (More to do later.)

--- js/index.js
+++ js/index.js
@@ -5,3 +5,35 @@ function openMenu() {
 function closeMenu() {
     document.getElementById("mineMenu").style.width= "0%"
 }
+
+function openNewMenu() {
+    document.getElementById("theNewMenu").style.width= "200px"
+}
+
+function closeNewMenu() {
+    document.getElementById("theNewMenu").style.width= "0px"
+}
+
+
+async function NewMenu(){
+    var currentvalue = document.getElementById('newButtonMenu').value;
+    if(currentvalue == "v"){
+      document.getElementById("newButtonMenu").value="V";
+      document.getElementById("newButtonMenu").style.fontSize="1em";
+      document.getElementById("newButtonMenu").style.transform = "rotate(180deg)";
+      document.getElementById("theNewMenuLine").style.width= "200px";
+      document.getElementById("theNewMenuLine").style.height= "100px";
+      document.getElementById("theNewMenu").style.display = "block";
+      await new Promise(r => setTimeout(r, 100));
+      document.getElementById("theNewMenu").style.opacity = "100%";
+    }else{
+      document.getElementById("newButtonMenu").value="v";
+      document.getElementById("newButtonMenu").style.transform = "rotate(0deg)";
+      document.getElementById("theNewMenu").style.opacity = "0%";
+      await new Promise(r => setTimeout(r, 100));
+      document.getElementById("theNewMenuLine").style.width= "125px";
+      document.getElementById("theNewMenuLine").style.height= "50px";
+      document.getElementById("newButtonMenu").style.fontSize="1.5em";
+      document.getElementById("theNewMenu").style.display = "none";
+    }
+}
